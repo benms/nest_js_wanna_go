81870c4865a5fc60cbffdb208f9bc8882935c370
Dmytro Ben <dmitriy.ben@gmail.com>
2021-05-18 23:59:52 +0300

    feat(nest): config/env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
