a206582f002aeb241ff4bb1adbfc9482e5803ef2
Dmytro Ben <dmitriy.ben@gmail.com>
2021-05-30 20:20:25 +0300

    feat(transformer): added null interceptor, added es2019 tscript support

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -39,6 +39,7 @@ export class AuthenticationController {
     const cookie = this.authenticationService.getCookieWithJwtToken(user.id);
     response.setHeader('Set-Cookie', cookie);
     user.password = undefined;
+
     return response.send(user);
   }
 
